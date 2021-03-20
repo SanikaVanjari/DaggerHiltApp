23a20be1e9458d3592438d6b112ad3314b292e45
Sanika Vanjari <sanikavanjari94@gmail.com>
2021-03-20 23:23:34 +0530

    injecting into activity

--- app/src/main/java/com/practice/daggerhiltapp/MainActivity.kt
+++ app/src/main/java/com/practice/daggerhiltapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.practice.daggerhiltapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var testString: String
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "Test String: $testString")
     }
 }
