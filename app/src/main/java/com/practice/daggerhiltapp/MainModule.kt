6a3ab45d30d6bcb12fa525dece1ba3d153e93787
Sanika Vanjari <sanikavanjari94@gmail.com>
2021-03-20 23:50:43 +0530

    to get context in module

--- app/src/main/java/com/practice/daggerhiltapp/MainModule.kt
+++ app/src/main/java/com/practice/daggerhiltapp/MainModule.kt
@@ -1,10 +1,12 @@
 package com.practice.daggerhiltapp
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Named
 import javax.inject.Singleton
@@ -15,6 +17,6 @@ object MainModule {
     @ActivityScoped
     @Provides
     @Named("String2")
-    fun provideTestString2() = "This is a string we will inject"
+    fun provideTestString2(@ApplicationContext context: Context) = context.getString(R.string.string_to_be_injected)
 
 }
